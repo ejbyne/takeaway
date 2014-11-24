7b7a31a14ce28b12c7b9c8a6ef636c76da40abf9
ed <ejbyne@gmail.com>
2014-11-24 15:58:05 +0000

    amended send message method to allow better testing

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,6 +6,7 @@ class Takeaway
   TWILIO_AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
   TWILIO_NUMBER = ENV['TWILIO_NUMBER']
   MY_NUMBER = ENV['MY_NUMBER']
+  CLIENT = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 
   attr_reader :orders
 
@@ -19,17 +20,16 @@ class Takeaway
     self.send_message(MY_NUMBER)
     # using my mobile number for test purposes
     # self.send_message(order.customer_telephone_number)
-    # using customer telephone number for production version
+    # would use customer telephone number for production version
   end
 
-  def send_message(customer_telephone_number)
+  def send_message(customer_telephone_number, client = CLIENT)
     delivery_time = Time.now + 3600
-    @client = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
-    @client.messages.create(
-        :from => TWILIO_NUMBER,
-        :to => customer_telephone_number,
-        :body => "Thank you! Your order was placed and will be delivered before #{delivery_time.hour}:#{delivery_time.min}"
-      )
+    client.messages.create(
+      :from => TWILIO_NUMBER,
+      :to => customer_telephone_number,
+      :body => "Thank you! Your order was placed and will be delivered before #{delivery_time.hour}:#{delivery_time.min}"
+    )
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,6 +5,8 @@ describe Takeaway do
   let (:takeaway) { Takeaway.new }
   let (:order) { double :order, total: 7 }
   let (:customer) { double :customer, telephone_number: '+441234567890' }
+  let (:client) { double :client, messages: messages }
+  let (:messages) { double :messages }
 
   it "should be able to receive an order if the payment amount is correct" do
     allow(takeaway).to receive(:send_message) # stubs out send_message method
@@ -14,6 +16,11 @@ describe Takeaway do
 
   it "should raise an error if the payment amount does not equal the order total" do
     expect(lambda { takeaway.receive(order, 6) }).to raise_error(RuntimeError)
+  end
+
+  it "should be able to send a confirmation text message" do
+    allow(messages).to receive(:create)
+    takeaway.send_message('+441234567890', client)
   end
 
 end
